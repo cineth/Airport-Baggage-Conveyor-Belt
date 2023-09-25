cb492ab4e440523ff31d1d773df6bc49a5bf0455
cineth <cinethmarakwatte@cmail.carleton.ca>
2023-09-24 23:36:53 -0400

    Update

--- ConveyorBelt.java
+++ ConveyorBelt.java
@@ -1,4 +1,24 @@
 public class ConveyorBelt {
+    private Node head;
+    private Node tail;
+    public ConveyorBelt() {
+        this.head = null;
+        this.tail = null;
+    }
 
+    // Add a bag to the conveyor belt
+    public void checkBag(Baggage baggage) {
+        Node newNode = new Node(baggage);
+        if (head == null) {
+            head = tail = newNode;
+        } else {
+            newNode.setPrev(tail);
+            tail.setNext(newNode);
+            tail = newNode;
+        }
+    }
+    public void checkBag(){
+
+    }
 
 }
